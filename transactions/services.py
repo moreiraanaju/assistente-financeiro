f6d58a337a089c9f77ef3c1b7983ac4bfc8330d0
morfimjvv <jvwmorfim@gmail.com>
2025-11-23 22:18:37 -0300

    feat: backend sprint 2

---
+++ transactions/services.py
@@ -0,0 +1,51 @@
+from unidecode import unidecode
+from .models import Categoria
+
+
+MAPEAMENTO = {
+    "Renda": [
+        "salario", "pix", "deposito", "venda", "bonus"
+    ],
+    "Alimentação": [
+        "mercado", "ifood", "restaurante", "lanche", "pizza", "padaria",
+        "almoco", "jantar", "acai"
+    ],
+    "Transporte": [
+        "uber", "99", "taxi", "onibus", "gasolina", "posto", "estacionamento"
+    ],
+    "Casa": [
+        "luz", "agua", "internet", "gas", "condominio", "claro", "vivo", "net"
+    ],
+    "Saúde": [
+        "farmacia", "remedio", "medico", "consulta", "exame"
+    ],
+    "Lazer": [
+        "cinema", "bar", "viagem", "netflix", "spotify", "ingresso"
+    ],
+    "Compras": [
+        "shopee", "amazon", "roupa", "loja", "shein", "presente"
+    ],
+    "Educação": [
+        "faculdade", "curso", "escola", "livro", "aula"
+    ],
+    "Investimento": [
+        "cdb", "poupanca", "tesouro", "cripto"
+    ],
+}
+
+ 
+def normalizar_texto(texto: str) -> str:           #coloca as palavras no padrão
+    return unidecode(texto).lower().strip()
+
+
+def identificar_categoria(descricao: str) -> Categoria:
+    descricao_normalizada = normalizar_texto(descricao)       
+    for categoria, palavras in MAPEAMENTO.items():
+        for palavra in palavras:
+            if palavra in descricao_normalizada:
+                try:
+                    return Categoria.objects.get(nome=categoria)
+                except Categoria.DoesNotExist:        
+                    pass 
+
+    return Categoria.objects.get(nome="Outros")
