32e1b6194fee38234fe23aa04139b2051848a628
Ana Ju <ferreiraanajussara@gmail.com>
2025-12-01 21:50:15 -0300

    feat: visualiza dados via django admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Transacao, Category
 
-# Register your models here.
+# Configuração para visualizar as Transações
+@admin.register(Transacao)
+class TransacaoAdmin(admin.ModelAdmin):
+    # O que vai aparecer nas colunas da lista
+    list_display = ('id', 'description', 'value', 'type', 'category', 'date_transaction')
+    
+    # Filtros laterais para facilitar a busca
+    list_filter = ('type', 'date_transaction', 'category')
+    
+    # Campo de busca
+    search_fields = ('description',)
+
+# Configuração para visualizar as Categorias
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
